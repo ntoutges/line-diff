1489542ac18efa74ed0288f19990b55660306f9d
Nicholas Toutges <ntoutges@gmail.com>
2024-04-10 20:26:00 -0600

    difference engine now outputs missing lines

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+// set these to null to have nothing load in
+const defaultPattern = "^([^ ]+?) +(.+?)  +(.+?) +(.+?)  +(.+?) .*$";
+const defaultSym = "1,2,4";
+
 function init() {
-  const oldPattern = localStorage.getItem("pattern");
+  const oldPattern = localStorage.getItem("pattern") ?? defaultPattern;
   if (oldPattern !== null) {
     $("#regex-pattern").value = oldPattern;
     adjustInputWidth.call($("#regex-pattern"));
     createPattern(oldPattern);
   }
-  const oldSymGroups = localStorage.getItem("sym");
+  const oldSymGroups = localStorage.getItem("sym") ?? defaultSym;
   if (oldSymGroups !== null) {
     setSymPattern(oldSymGroups.split(",").map(num => +num));
   }
 
   tabChange("text");
   Array.from($$(".tabs")).forEach(tab => {
-    tab.addEventListener("click", function() {
+    tab.addEventListener("click", function () {
       tabChange(this.getAttribute("value"));
     });
   });
 
-  $("#regex-pattern").addEventListener("input", function() {
+  $("#regex-pattern").addEventListener("input", function () {
     adjustInputWidth.call(this);
     savePattern(this.value);
     createPattern(this.value);
@@ -32,6 +36,12 @@ function init() {
 
   $("#input-a").addEventListener("input", inputChange);
   $("#input-b").addEventListener("input", inputChange);
+
+  const inputAVal = localStorage.getItem("input-a");
+  const inputBVal = localStorage.getItem("input-b");
+  if (inputAVal !== null) $("#input-a").value = inputAVal;
+  if (inputBVal !== null) $("#input-b").value = inputBVal;
+  if (inputAVal !== null || inputBVal !== null) inputChange(); // update
 }
 
 var lastTab = null;
@@ -52,19 +62,19 @@ function tabChange(tab) {
 function adjustInputWidth() {
   const span = document.createElement('span');
   span.textContent = this.value || this.placeholder;
-  
+
   const inputStyles = window.getComputedStyle(this);
   const fontProperties = ['font-family', 'font-size', 'font-weight', 'font-style', 'font-variant'];
   for (const prop of fontProperties) {
     span.style[prop] = inputStyles[prop];
   }
-  
+
   span.classList.add("text-measurer");
   document.body.appendChild(span); // Append the span to the body
-  
+
   const width = span.getBoundingClientRect().width;
   this.style.width = width + 'px';
-  
+
   // Remove the temporary span element
   document.body.removeChild(span);
 }
@@ -84,23 +94,23 @@ function createPattern(patternStr) {
     $("#warning").textContent = "";
     inputChange();
   }
-  catch(err) {
+  catch (err) {
     $("#warning").textContent = "Invalid Pattern";
   }
 }
 
 function populateSymPattern(len) {
   $("#matching-ids").innerHTML = "";
-  
+
   for (let i = 1; i <= len; i++) {
     const el = document.createElement("div");
     el.classList.add("sym-groups")
     el.textContent = i;
     el.setAttribute("value", i);
-    
+
     $("#matching-ids").append(el);
 
-    el.addEventListener("click", function() {
+    el.addEventListener("click", function () {
       this.classList.toggle("selected");
       if (this.classList.contains("selected")) symGroups.add(+this.getAttribute("value"));
       else symGroups.delete(+this.getAttribute("value"));
@@ -120,8 +130,8 @@ function populateSymPattern(len) {
 function setSymPattern(indicesSet) {
   const childs = $("#matching-ids").children;
   for (const i of indicesSet) {
-  const childI = +i - 1; // [indicesSet] starts at 1 because RegEx
-  if (childs.length < childI || childI < 0) continue;
+    const childI = +i - 1; // [indicesSet] starts at 1 because RegEx
+    if (childs.length < childI || childI < 0) continue;
     childs[childI].click();
   }
 }
@@ -135,13 +145,21 @@ function inputChange() {
   updateGrid();
 }
 
-function getDiffLines( aVal, bVal ) {
+function getDiffLines(aVal, bVal) {
+  if (pattern === null) {
+    return {
+      differing: [],
+      aMissing: [],
+      bMissing: []
+    };
+  }
+
   const a = aVal.map(line => pattern.exec(line));
   const b = bVal.map(line => pattern.exec(line));
 
-  const aIndices = new Set(a.map((_,i) => +i).filter(i => a[i] !== null));
-  const bIndices = new Set(b.map((_,i) => +i).filter(i => b[i] !== null));
-  
+  const aIndices = new Set(a.map((_, i) => +i).filter(i => a[i] !== null));
+  const bIndices = new Set(b.map((_, i) => +i).filter(i => b[i] !== null));
+
   const differingIndices = [];
 
   for (const i of aIndices) {
@@ -160,7 +178,7 @@ function getDiffLines( aVal, bVal ) {
         }
       }
       if (doPatternsMatch) { // non-matching lines
-        differingIndices.push([i,j]);
+        differingIndices.push([i, j]);
 
         // remove valid indices, as they have been used
         aIndices.delete(+i);
@@ -169,38 +187,102 @@ function getDiffLines( aVal, bVal ) {
       }
     }
   }
-  return differingIndices;
+
+  const { aMissing, bMissing } = getMissingIndices(
+    new Map(Array.from(aIndices).map(index => [index, a[index]])),
+    new Map(Array.from(bIndices).map((index) => [index, b[index]]))
+  );
+
+  return {
+    differing: differingIndices,
+    aMissing, bMissing
+  };
+}
+
+function getMissingIndices(
+  aMap, bMap
+) {
+  // note: values cannot contain '\n' char, so that will be used to deliniate groups
+  const aGroups = new Map();
+  const bGroups = new Map();
+  aMap.forEach((val, index) => {
+    let groups = [];
+    for (let i = 1; i < val.length; i++) { groups.push(val[i]); } // reduce regex output to array
+    const key = groups.join("\n");
+
+    if (!aGroups.has(key)) aGroups.set(key, []);
+    aGroups.get(key).push({ index, groups });
+  });
+
+  bMap.forEach((val, index) => {
+    let groups = [];
+    for (let i = 1; i < val.length; i++) { groups.push(val[i]); } // reduce regex output to array
+    const key = groups.join("\n");
+
+    if (!bGroups.has(key)) bGroups.set(key, []);
+    bGroups.get(key).push({ index, groups });
+  });
+
+  aGroups.forEach((aEntries, key) => {
+    if (!bGroups.has(key)) return; // ignore
+
+    const aLen = aEntries.length;
+    const bLen = bGroups.get(key).length;
+
+    // remove as many repeat lines as possible
+    const delCt = Math.min(aLen, bLen);
+    for (let i = 0; i < delCt; i++) {
+      aGroups.get(key).pop();
+      bGroups.get(key).pop();
+    }
+
+    if (aGroups.get(key).length == 0) aGroups.delete(key);
+    if (bGroups.get(key).length == 0) bGroups.delete(key);
+  });
+
+  // all duplicates removed
+  return {
+    aMissing: Array.from(aGroups.values()).map((entries) => entries.map(({ index }) => index)).flat(),
+    bMissing: Array.from(bGroups.values()).map((entries) => entries.map(({ index }) => index)).flat()
+  }
 }
 
 function showDiffLines(diffIndices, aVal, bVal) {
   $("#text-output").innerHTML = ""; // remove all children
-  diffIndices.forEach(([ai, bi]) => {
+  diffIndices.differing.forEach(([ai, bi]) => { createLine(ai, bi, "differences"); });
+  diffIndices.aMissing.forEach((ai) => { createLine(ai, -1, "missings"); });
+  diffIndices.bMissing.forEach((bi) => { createLine(-1, bi, "missings"); });
+
+  function createLine(ai, bi, classname) {
     const aNumEl = document.createElement("div");
     const bNumEl = document.createElement("div");
     aNumEl.classList.add("line-numbers");
     bNumEl.classList.add("line-numbers");
-    aNumEl.textContent = ai+1;
-    bNumEl.textContent = bi+1;
+    aNumEl.textContent = (ai >= 0) ? ai + 1 : "/";
+    bNumEl.textContent = (bi >= 0) ? bi + 1 : "/";
 
     const aLineEl = document.createElement("div");
     const bLineEl = document.createElement("div");
     aLineEl.classList.add("line-texts");
     bLineEl.classList.add("line-texts");
-    aLineEl.textContent = aVal[ai];
-    bLineEl.textContent = bVal[bi];
+    if (ai >= 0) aLineEl.textContent = aVal[ai];
+    if (bi >= 0) bLineEl.textContent = bVal[bi];
 
     const container = document.createElement("div");
-    container.classList.add("text-output-containers")
+    container.classList.add("text-output-containers", classname)
 
     container.append(aNumEl, aLineEl, bNumEl, bLineEl);
     $("#text-output").append(container);
-  });
+
+    aLineEl.addEventListener("click", () => { selectTextareaLine($("#input-a"), ai) });
+    bLineEl.addEventListener("click", () => { selectTextareaLine($("#input-b"), bi) });
+  }
 }
 
 function updateGrid() {
   const aData = $("#input-a").value.split("\n").map(line => pattern ? pattern.exec(line) : null);
   const bData = $("#input-b").value.split("\n").map(line => pattern ? pattern.exec(line) : null);
- 
+
   $("#grid-a").innerHTML = "";
   $("#grid-b").innerHTML = "";
   $("#grid-a").style.gridTemplateColumns = `repeat(${patternLen}, auto)`;
@@ -249,6 +331,54 @@ function updateGrid() {
     }
     $("#grid-b").append(container);
   }
+}
+
+// ctrl-c/ctrl-v: https://stackoverflow.com/questions/13650534/how-to-select-line-of-text-in-textarea
+function selectTextareaLine(tarea, lineNum) {
+  if (lineNum <= 0) return;
+
+  var lines = tarea.value.split("\n");
+
+  // calculate start/end
+  var startPos = 0, endPos = tarea.value.length;
+  for (var x = 0; x < lines.length; x++) {
+    if (x == lineNum) {
+      break;
+    }
+    startPos += (lines[x].length + 1);
+
+  }
+
+  var endPos = lines[lineNum].length + startPos;
+
+  // do selection
+  // Chrome / Firefox
+
+  if (typeof (tarea.selectionStart) != "undefined") {
+    tarea.focus();
+    tarea.selectionStart = startPos;
+    tarea.selectionEnd = endPos;
+  }
+
+  // IE
+  if (document.selection && document.selection.createRange) {
+    tarea.focus();
+    tarea.select();
+    var range = document.selection.createRange();
+    range.collapse(true);
+    range.moveEnd("character", endPos);
+    range.moveStart("character", startPos);
+    range.select();
+  }
+
+  var lineHeight = tarea.scrollHeight / tarea.value.split("\n").length;
+  var jump = lineNum * lineHeight;
+  tarea.scrollTop = jump;
+}
+
+window.onbeforeunload = () => {
+  localStorage.setItem("input-a", $("#input-a").value);
+  localStorage.setItem("input-b", $("#input-b").value);
 }
 
 init();
